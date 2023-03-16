e70ce17039c12ec66c2260cc49c2d654aaf4cf60
Sky-seed0215 <k.nao02.ki15@gmail.com>
2023-03-16 21:37:15 +0900

    create get issues command

--- cog/tracker.py
+++ cog/tracker.py
@@ -1,6 +1,10 @@
 import requests
-from discord import app_commands, Interaction, Embed, Object
+import discord
+from discord import app_commands, Interaction, Embed, Object, Member
 from discord.ext import commands
+from typing import Optional, Literal
+import datetime
+import pytz
 
 import utils.sql_utils as sql
 from utils.sql_utils import DuplicateKeyException
@@ -35,17 +39,43 @@ class Tracker(commands.Cog):
             await interaction.response.send_message(f"Repository not found for the specified prefix `{prefix}`.")
 
     @group.command(name="get_all", description="Displays all registered repositories.")
-    async def get_all(self, interaction: Interaction):
+    async def get_all(self, interaction: Interaction, user: Member):
         embed = Embed(title="Repositories", color=0x1e90ff)
-        result = sql.User.get_all_repos(discord_id=interaction.user.id)
-        for i in result:
-            embed.add_field(name=f"Prefix: {i.prefix}", value=f"[{i.owner}/{i.repo}](https://github.com/{i.owner}/{i.repo})", inline=False)
-        await interaction.response.send_message(embed=embed)
+        result = sql.User.get_all_repos(discord_id=user.id)
+        if result == []:
+            await interaction.response.send_message("No repositories registered.")
+        else:
+            for i in result:
+                embed.add_field(name=f"Prefix: {i.prefix}", value=f"[{i.owner}/{i.repo}](https://github.com/{i.owner}/{i.repo})", inline=False)
+            await interaction.response.send_message(embed=embed)
 
     @group.command(name="remove_all", description="Removes all repositories.")
     async def remove_all_repo(self, interaction: Interaction):
         sql.User.remove_all(discord_id=interaction.user.id)
         await interaction.response.send_message("All registered repositories have been deleted.")
+
+    @group.command(name="issue", description="Displays the specified Issue.")
+    async def get_issue(self, interaction: Interaction, prefix: str, number: int, multiple: Optional[Literal["and"]], to: Optional[int]):
+        result = sql.User.get_repo(discord_id=interaction.user.id, prefix=prefix)
+        url = f"https://api.github.com/repos/{result.owner}/{result.repo}/issues"
+        if multiple == "and":
+            r = requests.get(url=f"{url}/{str(number)}")
+            r2 = requests.get(url=f"{url}/{str(to)}")
+            res = r.json()
+            res2 = r2.json()
+            embed = Embed(color=0x1e90ff, timestamp=datetime.datetime.now(pytz.timezone('Asia/Tokyo')))
+            embed.add_field(name=res["title"], value=f'{res["body"]}\n\n[open with github.com]({res["html_url"]})')
+            embed.add_field(name=res2["title"], value=f'{res2["body"]}\n\n[open with github.com]({res2["html_url"]})')
+            embed.set_footer(text=f'issue#{number}:{res["labels"][0]["name"]}, issue#{to}:{res2["labels"][0]["name"]}')
+            await interaction.response.send_message(embed=embed)
+        else:
+            r = requests.get(url=f"{url}/{str(number)}")
+            res = r.json()
+            embed = Embed(color=discord.Colour.from_str(value=f'#{res["labels"][0]["color"]}'), timestamp=datetime.datetime.now(pytz.timezone('Asia/Tokyo')))
+            embed.add_field(name=res["title"], value=f'{res["body"]}\n\n[open with github.com]({res["html_url"]})')
+            embed.set_footer(text=f'issue#{number}:{res["labels"][0]["name"]}')
+            await interaction.response.send_message(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Tracker(bot), guilds=[Object(id=840469180171550752)])
